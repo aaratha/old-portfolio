f0c9aaeed77f17cd7d85b2e1381c0940a70c2e5a
aaratha <101074206+aaratha@users.noreply.github.com>
2023-05-25 22:41:08 -0400

    big changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,31 @@
 import styles from './App.module.scss';
 import { Navigation } from './components/navigation/navigation';
 import Navigation_module from './components/navigation/navigation.module.scss';
-import { Routes, Route, MemoryRouter } from 'react-router-dom';
+import { Routes, Route, MemoryRouter, Router, BrowserRouter } from 'react-router-dom';
 import { Projects } from './components/projects/projects';
-import { Home } from './components/home/home';
-import { Link } from 'react-router-dom';
+import { Home } from './components/home';
+import { Sidebar } from './components/sidebar';
+import { ProjectsWeb } from './components/projects-web/projects-web';
+import { ProjectsVis } from './components/projects-vis/projects-vis';
+import { Contact } from './components/contact/contact';
+
 
 function App() {
     return (
-        <>
-            <div className={styles.App}>
-                <Navigation />
+        <MemoryRouter>
+            <div className='flex flex-row gradbg'>
+                <Sidebar />
+                <div className='flex w-[85%] border border-white border-double'>
+                    <Routes>
+                        <Route path="/" element={<Home />} />
+                        <Route path="/projects/*" element={<Projects />} />
+                        <Route path="/projects-vis" element={<ProjectsVis />} />
+                        <Route path="/projects-web" element={<ProjectsWeb />} />
+                        <Route path="/contact" element={<Contact />} />
+                    </Routes>
+                </div>
             </div>
-        </>
+        </MemoryRouter>
     );
 }
 
