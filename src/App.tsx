327fbfacd13b73e401b8d4c20dcf0468320222f5
Aseem Ratha <aseemratha@gmail.com>
2023-05-30 16:28:55 -0400

    iframe bg

--- src/App.tsx
+++ src/App.tsx
@@ -13,16 +13,23 @@ function App() {
             <div className="flex flex-col md:flex-row font-nice-600">
                 <Sidebar />
                 <div className="inshad transition-shadow flex md:w-[85%] h-[88vh] md:h-screen bg-white body">
-                    <Routes>
-                        <Route path="/" element={<Home />} />
-                        <Route path="/projects" element={<Navigate to="/projects/videos" />} />
-                        <Route path="/projects/" element={<Projects />}>
-                            <Route path="/projects/videos" element={<Videos />} />
-                            <Route path="/projects/visuals" element={<Visuals />} />
-                            <Route path="/projects/websites" element={<ProjectsWeb />} />
-                        </Route>
-                        <Route path="/contact" element={<Contact />} />
-                    </Routes>
+                    <div className='z-[1] flex m-auto'>
+                        <Routes>
+                            <Route path="/" element={<Home />} />
+                            <Route path="/projects" element={<Navigate to="/projects/videos" />} />
+                            <Route path="/projects/" element={<Projects />}>
+                                <Route path="/projects/videos" element={<Videos />} />
+                                <Route path="/projects/visuals" element={<Visuals />} />
+                                <Route path="/projects/websites" element={<ProjectsWeb />} />
+                            </Route>
+                            <Route path="/contact" element={<Contact />} />
+                        </Routes>
+                    </div>
+                    <iframe
+                            className="absolute w-[85%] h-screen z-[0]"
+                            src="https://www.youtube.com/embed/TBMEBSfnJbQ?autoplay=1&mute=1&controls=0&showinfo=0&autohide=1&loop=2&controls=0&end=51&start=5"
+                            allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+                    ></iframe>
                 </div>
             </div>
         </MemoryRouter>
