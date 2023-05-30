b8ae7f60c554dbee57c1d15d34163ae85841e3ed
aaratha <101074206+aaratha@users.noreply.github.com>
2023-05-29 20:26:09 -0400

    more tweaks

--- src/components/contact.tsx
+++ src/components/contact.tsx
@@ -6,15 +6,15 @@ export interface ContactProps {
 
 export const Contact = ({ className }: ContactProps) => {
     return (
-        <div className="flex flex-col p-[2vw] border content-center align-center m-auto items-center justify-center hover:p-[3vw] transition-all duration-500">
-            <div className="flex flex-col m-auto pl-[10vw] pr-[10vw] pb-[7vw] pt-[7vw] hover:pl-[12vw] hover:pr-[12vw] hover:pb-[8vw] hover:pt-[8vw] border items-center justify-center transition-all duration-500">
-                <div className="flex flex-col m-auto p-[5vw] items-center bg-black justify-center transition-all duration-300 bg-opacity-60 hover:p-[6vw] hover:bg-opacity-80 text-white">
+        <div className="flex flex-col p-[2vw] border border-secondary content-center align-center m-auto items-center justify-center hover:p-[3vw] transition-all duration-500 ">
+            <div className="flex flex-col m-auto pl-[10vw] pr-[10vw] pb-[7vw] pt-[7vw] hover:pl-[12vw] hover:pr-[12vw] hover:pb-[8vw] hover:pt-[8vw] border border-secondary items-center justify-center transition-all duration-500">
+                <div className="flex flex-col m-auto p-[5vw] items-center bg-secondary justify-center transition-all duration-300 bg-opacity-60 hover:p-[6vw] hover:bg-opacity-80">
                     <a
                         href="https://www.instagram.com/aa.ratha/"
                         style={{ textDecoration: 'none', color: 'white' }}
                     >
-                        <div className="text-xl flex items-center justify-center mb-1">
-                            <img src={icon1} className="w-8 mr-2" />
+                        <div className="hover:scale-110 transition-all text-primary text-xl flex items-center justify-center mb-1">
+                            <img src={icon1} className="fill-primary w-8 mr-2" />
                             @aa.ratha
                         </div>
                     </a>
@@ -22,8 +22,8 @@ export const Contact = ({ className }: ContactProps) => {
                         href="mailto:aseemratha@gmail.com"
                         style={{ textDecoration: 'none', color: 'white' }}
                     >
-                        <div className="flex text-xl items-center justify-center">
-                            <img src={icon2} className="w-10 mr-2" />
+                        <div className="hover:scale-110 transition-all text-primary flex text-xl items-center justify-center">
+                            <img src={icon2} className="fill-primary w-10 mr-2" />
                             aseemratha@gmail.com
                         </div>
                     </a>
