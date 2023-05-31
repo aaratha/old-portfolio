4f4305f0a589d50da67137618ab07f8e53075537
aaratha <101074206+aaratha@users.noreply.github.com>
2023-05-30 21:00:08 -0400

    color tweaks

--- tailwind.config.js
+++ tailwind.config.js
@@ -21,7 +21,8 @@ module.exports = {
         'primary': '#FFD6A8',
         /*'secondary': '#0d0703',*/
         'secondary': '#0E2039',
-        'tertiary':  '#6495ED',
+        'tertiary': '#1E457F',
+        /*'tertiary':  '#6495ED',*/
       },
       boxShadow: {
         '1': '0px 0px 15px -2px white'
