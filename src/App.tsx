43f8e109c4654a5b416e3022ae7856e8aea6cdd0
aaratha <101074206+aaratha@users.noreply.github.com>
2023-05-30 21:20:28 -0400

    minor

--- src/App.tsx
+++ src/App.tsx
@@ -27,7 +27,7 @@ function App() {
                         </Routes>
                     </div>
                     <iframe
-                        className="flex object-contain absolute translate-y-[-18vh] md:translate-y-0 top-[12vh] md:top-0 w-[300vw] sm:w-[220vw] md:w-[150vw] xl:w-[106vw] h-[100%] z-[0] overflow-hidden"
+                        className="flex object-contain absolute translate-y-[-16vh] md:translate-y-0 top-[12vh] md:top-0 w-[300vw] sm:w-[220vw] md:w-[150vw] xl:w-[106vw] h-[100%] z-[0] overflow-hidden"
                         src="https://www.youtube.com/embed/TBMEBSfnJbQ?autoplay=1&mute=1&controls=0&showinfo=0&autohide=1&loop=1&end=51&start=5&playlist=TBMEBSfnJbQ"
                         allow="autoplay; encrypted-media;"
                     ></iframe>
